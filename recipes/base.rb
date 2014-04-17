7830148043360a08d1357b0e49f1c153bdfa2e1b
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2014-04-17 16:06:05 -0300

    Remove `bundle exec` from rake commands that compile the assets

--- recipes/base.rb
+++ recipes/base.rb
@@ -31,9 +31,9 @@ namespace :deploy do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
       if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        %x{RAILS_ENV=#{rails_env} bundle exec rake assets:precompile}
+        %x{RAILS_ENV=#{rails_env} rake assets:precompile}
         %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{server_name}:#{shared_path}}
-        %x{bundle exec rake assets:clobber}
+        %x{rake assets:clobber}
       else
         logger.info 'Skipping asset pre-compilation because there were no asset changes'
       end
