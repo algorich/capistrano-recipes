de41a75a63ec8a99c1132679a232df846991b575
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2014-06-18 10:08:05 -0300

    adjust nginx recipe to create a sites-avalilable and a slink to sites-enabled

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -9,8 +9,9 @@ namespace :nginx do
 
   desc "Setup nginx configuration for this application"
   task :setup, roles: :web do
-    template "nginx_unicorn.erb", "/tmp/nginx_conf"
-    run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
+    template 'nginx_unicorn.erb', '/tmp/nginx_conf'
+    run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-available/#{application}"
+    run "#{sudo} ln -sf /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
     run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
     reload
   end
